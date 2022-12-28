741414574c636881d8c8bb5d3d37a495aee19abb
SnowflakeTr01 <121542438+Snowflaketr01@users.noreply.github.com>
2022-12-28 10:50:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import emoji
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast favourites')
 streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry oatmeal')
