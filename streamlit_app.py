f7b2a5b5ee7e144717d3e2efdfa786fafd9c31ec
SnowflakeTr01 <121542438+Snowflaketr01@users.noreply.github.com>
2022-12-28 10:50:18 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
+import emoji
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.write('Omega 3 & Blueberry oatmeal')
-streamlit.write('Kale Spinach & Rocket Smoothie')
-streamlit.write('Hard Boiled free range egg')
+streamlit.header('Breakfast favourites')
+streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry oatmeal')
+streamlit.text(':green_salad: Kale Spinach & Rocket Smoothie')
+streamlit.text(':Chicken: Hard Boiled free range egg')
+streamlit.text(':Avacardo::bread: Avacardo toast')
